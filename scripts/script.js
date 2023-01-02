d18c163a36ea9a9d8d9097839600b7ef53926d56
Israel Paz <ipdevti@gmail.com>
2023-01-02 18:03:32 -0300

    1.0
    1.0

---
+++ scripts/script.js
@@ -0,0 +1,78 @@
+const FRONT = "card-front";
+const BACK = "card-back";
+const CARD = "card";
+const ICON = "icon";
+const FLIP = "flip";
+
+startGame();
+
+function startGame(){
+    initializeCards(game.createCards());
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.querySelector(".game-board");
+    gameBoard.innerHTML = "";
+    game.cards.forEach(card => {        
+        let cardElement = document.createElement("div");
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+        createCardContent(card, cardElement);
+        cardElement.addEventListener("click", flipCard);
+        gameBoard.appendChild(cardElement);
+    })
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement("div");
+    cardElementFace.classList.add(face);
+
+    if (face == FRONT) {
+        let iconElement = document.createElement("img");
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt";
+    }
+    element.appendChild(cardElementFace);
+}
+
+function flipCard() {
+    if (game.setCard(this.id)) {
+        this.classList.add(FLIP);
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards();
+                if (game.checkGameOver()) {
+                    let gamerOverLayer = document.querySelector(".game-over");
+                    gamerOverLayer.style.display = "flex";
+                }
+            }else {
+                setTimeout(() => { 
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+                    firstCardView.classList.remove("flip");
+                    secondCardView.classList.remove("flip");
+                    game.unflipCards();
+                }, 1000)
+            }
+        }
+    }
+}
+
+let restart = document.querySelector(".restart");
+restart.addEventListener("click", restartGame);
+
+function restartGame() {
+    game.clearCards();
+    startGame();
+    let gamerOverLayer = document.querySelector(".game-over");
+    gamerOverLayer.style.display = "none";
+}
